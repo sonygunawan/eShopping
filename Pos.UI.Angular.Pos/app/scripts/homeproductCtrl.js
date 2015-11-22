cf3b28599dbd18922865e413da6c6f3be67a3917
sonygunawan <sony_ss4@yahoo.com>
2015-11-22 10:24:45 +0700

    routeParam success

--- Pos.UI.Angular.Pos/app/scripts/homeproductCtrl.js
+++ Pos.UI.Angular.Pos/app/scripts/homeproductCtrl.js
@@ -1,9 +1,13 @@
 var pos = angular.module('pos');
 
-pos.controller('homeproductCtrl', function ($scope, $http) {
+pos.controller('homeproductCtrl', function ($scope, $routeParams, $location, $http) {
     var url = "http://localhost:3677/api/product";
     $scope.selectedEntity = {};
+    //$scope.idParam = $routeParams.categoriesId;
+    $scope.idParam = "TestidParam";
+    $scope.categoriesIdParam = $routeParams.categoriesId;
     
+    //$location.path('/tasks/' + idParam + '/edit/');
     $scope.selectEntity = function (entity) {
         $scope.selectedEntity = entity;
     }
